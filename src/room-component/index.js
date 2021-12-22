7546f7be881eaab0c7a70a22be1f1672fa503f15
LiuQ-Q <545784977@qq.com>
2021-12-22 11:40:46 +0800

    PIXI.Texture.fromBuffer

--- src/room-component/index.js
+++ src/room-component/index.js
@@ -6,66 +6,76 @@ window.PIXI = PIXI;
 const WIDTH = 100;
 const HEIGHT = 100;
 
-const app = new PIXI.Application({
-    width: 1000,
-    height: 1000,
-    backgroundColor: 0xeef0f1,
-    antialias: true,    // default: false 反锯齿
-});
+window.addEventListener('load', async function () {
+    const app = new PIXI.Application({
+        width: 1000,
+        height: 1000,
+        backgroundColor: 0xeef0f1,
+        antialias: true,    // default: false 反锯齿
+    });
 
-document.body.appendChild(app.view);
+    document.body.appendChild(app.view);
 
-// const texture = PIXI.Texture.from(spider, {
-//     width: 200,
-//     height: 200
-// });
-// const texture = new Promise(resolve => {
-//     const spiderTexture = PIXI.Texture.from(spider, {
-//         width: 100,
-//         height: 100
-//     });
-//     resolve(spiderTexture);
-// })
+    // const texture = PIXI.Texture.from(spider, {
+    //     width: 200,
+    //     height: 200
+    // });
 
-const texture = PIXI.Texture.fromURL('https://qhyxpicoss.kujiale.com/r/2020/03/30/L4D1111ENDDVAXSVUGEJKFNNVCQD3WKVY8_199x225.png');
+    const textureUrl = await PIXI.Texture.fromURL('https://qhyxpicoss.kujiale.com/r/2020/03/30/L4D1111ENDDVAXSVUGEJKFNNVCQD3WKVY8_199x225.png');
 
-const graphics = new PIXI.Graphics();
+    console.log(textureUrl, 'url')
+    
+    const img = await window.fetch('https://qhcmsoss.kujiale.com/ttex/L2HNXEIKN4FT2AABAAAAAAY8.jpg', {
+        method: 'GET',
+        responseType: 'blob'
+    }).then(res => {
+        return res.blob();
+    }).then(blob => {
+        return blob.arrayBuffer();
+    })
 
-window.graphics = graphics;
+    console.log(img, 'img')
 
-app.stage.addChild(graphics);
+    const textureBlob = PIXI.Texture.fromBuffer(img, 200, 225);
 
-graphics
-    // .beginFill(0x000000)
-    .drawRect(0, 0, WIDTH, HEIGHT)
-    .endFill()
+    console.log(textureBlob, 'blob')
+    
+    // let texture = null
 
-graphics.pivot.x = WIDTH / 2;
-graphics.pivot.y = HEIGHT / 2;
-
-
-
-
-texture.then((res) => {
+    // const xhr = new XMLHttpRequest();
+    // xhr.onreadystatechange = function() {
+    //   if (this.readyState === 4 && this.status === 200) {
+    //     console.log(this.response, 'xhr');
+    //   }
+    // };
+    // xhr.responseType = "blob";
+    // xhr.open("GET", "https://qhcmsoss.kujiale.com/ttex/L2HNXEIKN4FT2AABAAAAAAY8.jpg", true);
+    // xhr.send();
+    
+    const graphics = new PIXI.Graphics();
+    
+    window.graphics = graphics;
+    
+    app.stage.addChild(graphics);
+    
+    graphics
+        .drawRect(0, 0, WIDTH, HEIGHT)
+        .endFill()
+    
+    graphics.pivot.x = WIDTH / 2;
+    graphics.pivot.y = HEIGHT / 2;
+    
     graphics
         .beginTextureFill({
-            texture: res,
-            matrix: new PIXI.Matrix(0.5, 0, 0, 0.5, 0, 0)
+            texture: textureBlob,
+            // matrix: new PIXI.Matrix(0.5, 0, 0, 0.5, 0, 0)
         })
-        .drawRect(0, 0, WIDTH, HEIGHT)
+        .drawShape(textureBlob.orig)
+        // .drawRect(0, 0, WIDTH, HEIGHT)
     
-    // graphics.width = WIDTH;
-    // graphics.height = HEIGHT;
+    graphics.x = 100;
+    graphics.y = 100;
+    
+    // graphics.width = 100;
+    // graphics.height = 100;
 })
-
-// graphics
-//     .beginTextureFill({
-//         texture
-//     })
-//     .drawShape(texture.orig)
-
-graphics.x = 100;
-graphics.y = 100;
-
-// graphics.width = 100;
-// graphics.height = 100;
